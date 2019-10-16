41fd5fd8cf13ba7f839ba8d9815117ee51eaa638
Lalith Suresh <lsuresh@vmware.com>
2019-10-16 14:57:16 -0700

    Try circleci kind set up
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

---
+++ kind-smoketest.sh
@@ -0,0 +1,42 @@
+!/bin/bash
+
+# standard bash error handling
+set -o errexit;
+set -o pipefail;
+set -o nounset;
+# debug commands
+set -x;
+
+# working dir to install binaries etc, cleaned up on exit
+BIN_DIR="$(mktemp -d)"
+# kind binary will be here
+KIND="${BIN_DIR}/kind"
+
+# cleanup on exit (useful for running locally)
+cleanup() {
+    "${KIND}" delete cluster || true
+    rm -rf "${BIN_DIR}"
+}
+trap cleanup EXIT
+
+# util to install a released kind version into ${BIN_DIR}
+install_kind_release() {
+    VERSION="v0.5.1"
+    KIND_BINARY_URL="https://github.com/kubernetes-sigs/kind/releases/download/${VERSION}/kind-linux-amd64"
+    wget -O "${KIND}" "${KIND_BINARY_URL}"
+    chmod +x "${KIND}"
+}
+
+main() {
+    # get kind
+    install_kind_release
+    # create a cluster
+    "${KIND}" create cluster --loglevel=debug
+    # set KUBECONFIG to point to the cluster
+    KUBECONFIG="$("${KIND}" get kubeconfig-path)"
+    export KUBECONFIG
+    # TODO: invoke your tests here
+    # teardown will happen automatically on exit
+}
+
+main
