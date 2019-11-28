1577a673263e5d39c3754573c48d96500967a433
Lalith Suresh <lsuresh@vmware.com>
2019-11-27 17:33:51 -0800

    Test dependency install
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- deps.sh
+++ deps.sh
@@ -18,6 +18,7 @@ export PATH=`pwd`/apache-maven-3.6.2-bin/bin:$PATH
 export PATH=`pwd`/jdk-12.0.2/bin:$PATH
 export PATH=`pwd`/MiniZincIDE-2.3.2-bundle-linux/bin/:$PATH
 export JAVA_HOME=`pwd`/jdk-12.0.2/
+export OR_TOOLS_LIB=`pwd`/or-tools_Ubuntu-18.04-64bit_v7.4.7247/lib/libjniortools.so
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/MiniZincIDE-2.3.2-bundle-linux/lib
 export QT_PLUGIN_PATH=$QT_PLUGIN_PATH:`pwd`/MiniZincIDE-2.3.2-bundle-linux/plugins
 
