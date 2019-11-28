97c6dde749dcfadd85b9c560fcea5e1136d2bf09
Lalith Suresh <lsuresh@vmware.com>
2019-11-27 17:21:22 -0800

    Test dependency install
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- deps.sh
+++ deps.sh
@@ -14,13 +14,14 @@ tar -xzvf openjdk-12.0.2_linux-x64_bin.tar.gz
 tar -xzvf or-tools_ubuntu-18.04_v7.4.7247.tar.gz
 tar -xzvf MiniZincIDE-2.3.2-bundle-linux-x86_64.tgz
 
-
 export PATH=`pwd`/apache-maven-3.6.2-bin/bin:$PATH
 export PATH=`pwd`/jdk-12.0.2/bin:$PATH
 export PATH=`pwd`/MiniZincIDE-2.3.2-bundle-linux/bin/:$PATH
 export JAVA_HOME=`pwd`/jdk-12.0.2/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/MiniZincIDE-2.3.2-bundle-linux/lib
 export QT_PLUGIN_PATH=$QT_PLUGIN_PATH:`pwd`/MiniZincIDE-2.3.2-bundle-linux/plugins
+
+(cd or-tools_ubuntu-18.04_v7.4.7247.tar.gz/lib ; mvn install:install-file -Dfile=com.google.ortools.jar -DgroupId=com.google -DartifactId=ortools -Dversion=7.4 -Dpackaging=jar)
 
 mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package
 mvn -pl '!build-common' jacoco:report
