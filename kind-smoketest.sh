ce5e238d9d7a145c571e473fe9d4316088fdf410
Lalith Suresh <lsuresh@vmware.com>
2019-11-27 17:43:57 -0800

    Test dependency install
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- kind-smoketest.sh
+++ kind-smoketest.sh
@@ -43,6 +43,20 @@ main() {
     # sudo mv ./kubectl /usr/local/bin/kubectl
     # kubectl version
     # kubectl cluster-info
+
+    export PATH=`pwd`/apache-maven-3.6.2-bin/bin:$PATH
+    export PATH=`pwd`/jdk-12.0.2/bin:$PATH
+    export PATH=`pwd`/MiniZincIDE-2.3.2-bundle-linux/bin/:$PATH
+    export JAVA_HOME=`pwd`/jdk-12.0.2/
+    export OR_TOOLS_LIB=`pwd`/or-tools_Ubuntu-16.04-64bit_v7.4.7247/lib/libjniortools.so
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/MiniZincIDE-2.3.2-bundle-linux/lib
+    export QT_PLUGIN_PATH=$QT_PLUGIN_PATH:`pwd`/MiniZincIDE-2.3.2-bundle-linux/plugins
+    
+    (cd or-tools_Ubuntu-16.04-64bit_v7.4.7247/lib/ ; mvn install:install-file -Dfile=com.google.ortools.jar -DgroupId=com.google -DartifactId=ortools -Dversion=7.4 -Dpackaging=jar)
+    
+    mvn clean org.jacoco:jacoco-maven-plugin:prepare-agent package
+    mvn -pl '!build-common' jacoco:report
+    bash <(curl -s https://codecov.io/bash)
 }
 
 main
